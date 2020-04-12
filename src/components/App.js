e9ada695cf3d234cce966c339b0d3fc2cb3c1c02
Otsdarva1 <yamazaki192748@gmail.com>
2020-04-13 01:35:06 +0900

    modify tab to space

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,14 @@ import Events from './Events'
 import reducer from '../reducers'
 
 const App = () => {
-	const [state, dispatch] = useReducer(reducer, [])
+  const [state, dispatch] = useReducer(reducer, [])
 
-	return (
-		<div className="container-fluid">
-			<EventForm state={state} dispatch={dispatch} />
-			<Events state={state} dispatch={dispatch} />
-		</div>
-	)
+  return (
+    <div className="container-fluid">
+      <EventForm state={state} dispatch={dispatch} />
+      <Events state={state} dispatch={dispatch} />
+    </div>
+  )
 }
 
 export default App
