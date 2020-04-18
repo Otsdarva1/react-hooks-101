33c52025bfce4e7f918ba028b4df3b19ace42855
Otsdarva1 <yamazaki192748@gmail.com>
2020-04-18 22:14:50 +0900

    replace type names with constants

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,18 @@
+import {
+    CREATE_EVENT,
+    DELETE_EVENT,
+    DELETE_ALL_EVENTS
+  } from '../actions'
+
 const events = (state = [], action) => {
     switch(action.type) {
-        case 'CREATE_EVENT':
+        case CREATE_EVENT:
             let id = state.length === 0 ? 1 : state[state.length - 1].id + 1
             const event = { id, title: action.title, body: action.body }
             return [...state, event]
-        case 'DELETE_EVENT':
+        case DELETE_EVENT:
             return state.filter(event => event.id !== action.id)
-        case 'DELETE_ALL_EVENTS':
+        case DELETE_ALL_EVENTS:
             return []
         default:
             return state
