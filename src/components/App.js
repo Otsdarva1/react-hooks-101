d2ff8dc0786eb5bb7ffea117a5d5c00c63173d16
Otsdarva1 <yamazaki192748@gmail.com>
2020-04-21 15:14:37 +0900

    refactor events with reducer

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ import AppContext from '../contexts/AppContext'
 import EventForm from './EventForm'
 import Events from './Events'
 
-import reducer from '../reducers'
+import reducer from '../reducers/index'
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, [])
+  const initialState = {
+    events: []
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <AppContext.Provider value={{state, dispatch}}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,4 @@
-import {
-    CREATE_EVENT,
-    DELETE_EVENT,
-    DELETE_ALL_EVENTS
-  } from '../actions'
+import { combineReducers } from 'redux';
+import events from './events';
 
-const events = (state = [], action) => {
-    switch(action.type) {
-        case CREATE_EVENT:
-            let id = state.length === 0 ? 1 : state[state.length - 1].id + 1
-            const event = { id, title: action.title, body: action.body }
-            return [...state, event]
-        case DELETE_EVENT:
-            return state.filter(event => event.id !== action.id)
-        case DELETE_ALL_EVENTS:
-            return []
-        default:
-            return state
-    }
-}
-
-export default events
+export default combineReducers({ events })
