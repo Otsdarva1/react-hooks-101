50140a8a88dc31ef5be1015d7f585383f4a9ac69
Otsdarva1 <yamazaki192748@gmail.com>
2020-05-05 16:35:30 +0900

    delete all operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -29,7 +29,7 @@ const EventForm = () => {
     setBody('')
   }
 
-  const deleteAllEvents = (e) => {
+  const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しますか？')
     if (result) {
@@ -38,6 +38,16 @@ const EventForm = () => {
         type: ADD_OPERATION_LOG,
         description: '全てのイベントを削除しました。',
         operatedAt: timeCurrentIso8601
+      })
+    }
+  }
+
+  const deleteAllOperationLogs = e => {
+    e.preventDefault()
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？')
+    if (result) {
+      dispatch({
+        type: DELETE_ALL_OPERATION_LOGS
       })
     }
   }
@@ -57,6 +67,7 @@ const EventForm = () => {
         </div>
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
